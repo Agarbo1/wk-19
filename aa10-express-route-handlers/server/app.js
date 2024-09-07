68b790cc5a44331517fab5ecafc071a3f54f734e
Alex Garbarino <alexiothebrave@gmail.com>
2024-09-06 21:51:09 -0500

    commit

--- aa10-express-route-handlers/server/app.js
+++ aa10-express-route-handlers/server/app.js
@@ -24,9 +24,33 @@ const {
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log('Request Body:', req.body);
+  next();
+});
 
 // Your code here
+app.get('/artists', (req, res) => {
+  return res.json(getAllArtists())
+});
 
+app.get('artists/:artistId', (req, res) => {
+  return res.json(getArtistByArtistId(req.params.artistId));
+})
+
+app.post('/artists', (req, res) => {
+  return res.status(201).json(addArtist(req.body))
+})
+
+app.put('/artists/:artistId', (req, res) => {
+  return res.json(editArtistByArtistId(req.params.artistId, req.body))
+})
+
+app.delete("/artist/:artistID", (req, res) => {
+  deleteArtistByArtistId(req.params.artistID)
+
+  return res.json({ message: "Successfully Deleted"})
+})
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
